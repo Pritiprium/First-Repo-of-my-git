2f45bbfcd161c2c92a720e5e6f2384c3b17f81b8
priti prium <pritiprium23@gmail.com>
2020-04-14 19:28:53 -0500

    this is for priti branch

--- src/Git_demo.java
+++ src/Git_demo.java
@@ -9,13 +9,13 @@ public class Git_demo {
 		int a =10;
 		int b =20;
 		int c =a+b;
-		System.out.println("print");
+		System.out.println("print"+c);
 		System.out.println("i am creating one branch named priti");
 		
 //		new branch created
 //		this code is for now pankaj branch		
 		Scanner sc = new Scanner (System.in);
-		System.out.println("entr your name");
+		System.out.println("enter your name");
 		String name = sc.nextLine();
 	}
 
